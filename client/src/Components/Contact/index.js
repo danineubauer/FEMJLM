bff4c1f3b55f9875d89ca2cbe13965c8412f75cb
Danielle <danineubauerr@gmail.com>
2020-05-28 10:00:44 -0700

    adds backend

---
+++ client/src/Components/Contact/index.js
@@ -0,0 +1,124 @@
+import React, { useContext } from "react";
+import axios from 'axios';
+
+import './style.css'
+import Header from "../Header";
+
+
+
+class Contact extends React.Component {
+
+  constructor(props) { 
+    super(props);
+    this.state = { 
+      name: '', 
+      lastname: '',
+      email: '',
+      message: ''
+    }
+  }
+
+  
+
+
+   handleSubmit(event) { 
+    event.preventDefault(); 
+    console.log('form submited, ', this.state)
+
+
+
+
+  //   axios.post(`/send`,{data:'data'}).then((res)=>{
+  //     //on success
+  //     console.log('worked')
+  //     }).catch((error)=>{
+  //     //on error
+  //     console.log(error)
+  //     });
+  // }
+
+  // //another option: 
+
+  // const {name, email, message } = this.state
+  // const form = await axios.post('/api/form', { 
+  //   name, 
+  //   email, 
+  //   message
+  // })
+  
+  //   axios({
+  //     method: "POST", 
+  //     url:"http://localhost:3000", 
+  //     data:  this.state
+  //   }).then((response)=>{
+  //     if (response.data.status === 'success'){
+  //       alert("Message Sent."); 
+  //       this.resetForm()
+  //     }else if(response.data.status === 'fail'){
+  //       alert("Message failed to send.")
+  //     }
+  //   })
+  // }
+
+  // resetForm() { 
+  //   this.setState({name: '', email: '', message: ''})
+  // }
+   }
+  render() { 
+
+    return (
+      <div className="Contact">
+        <Header>
+          <p>We want to hear from you!</p>
+        </Header>
+        <div className='container'>
+  
+          <form id='contact-form' onSubmit={this.handleSubmit.bind(this)} method='POST' >
+            <div className="form-row form-group">
+              <div className="col">
+                <input type="text" className="form-control" placeholder="First name" value={this.state.name} onChange={this.onNameChange.bind(this)}/>
+              </div>
+              <div className="col">
+                <input type="text" className="form-control" placeholder="Last name" value={this.state.lastname} onChange={this.onLastnameChange.bind(this)}/>
+              </div>
+              <div className="col">
+                <input type="text" className="form-control" placeholder="Email" value={this.state.email} onChange={this.onEmailChange.bind(this)}/>
+              </div>
+            </div>
+            <br></br>
+  
+            <div class="form-group">
+              <label for="exampleFormControlTextarea1">Your message here:</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="10" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
+            </div>
+            <button type="submit" class="btn btn-primary">Send</button>
+  
+  
+          </form>
+        </div>
+  
+  
+      </div>
+    )
+  };
+
+  
+  onNameChange(event) { 
+    this.setState({name: event.target.value})
+  }
+
+  onLastnameChange(event) {
+    this.setState({lastname: event.target.value})
+  }
+
+  onEmailChange(event) { 
+    this.setState({email: event.target.value})
+  }
+
+  onMessageChange(event) { 
+    this.setState({message: event.target.value})
+  }
+
+}
+
+export default Contact;
---
+++ client/src/Components/Weekly/index.js
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import Header from '../Header/index';
+import './style.css';
+import Textbox from "../Textbox/index";
+
+function Weekly() {
+
+  return (
+    <div>
+      <Header>Meet Our Community</Header>
+
+      <div className="container">
+        <div className='row'>
+
+          <Textbox >
+
+            <h5 className="femstart-title">Fem Start of the Week:</h5><br></br>
+            <div className="fb-post" data-href="https://www.facebook.com/FemJLM/posts/3066955370027655" data-show-text="true" data-width=""><blockquote cite="https://developers.facebook.com/FemJLM/posts/3066955370027655" class="fb-xfbml-parse-ignore"><p>Elena Liberte is the Director for Strategic Partnerships and HR at Made in Jerusalem.  Elena has wide-ranging and deep...</p>Posted by <a href="https://www.facebook.com/FemJLM/">FEM JLM</a> on&nbsp;<a href="https://developers.facebook.com/FemJLM/posts/3066955370027655">Tuesday, May 19, 2020</a></blockquote></div>
+          </Textbox>
+          <Textbox>
+            Mentor matching engine
+          </Textbox>
+
+        </div>
+      </div>
+
+    </div>
+
+  );
+}
+
+export default Weekly;
